8d68b7d763b8dd721ce4d824f5cf69bc44c820a3
Francisco Cruz <cruzelante098@gmail.com>
2020-03-18 16:36:58 +0000

    Implements parsing of XML network file

---
+++ src/reader.ts
@@ -0,0 +1,38 @@
+import fs from 'fs';
+import BeautifulDom from 'beautiful-dom';
+import { Phase, TLLogic } from "./tl-logic";
+
+export function parseTlLogic(networkFilename: string): TLLogic[] {
+  const tlLogicArray: TLLogic[] = [];
+
+  const contents = fs.readFileSync(networkFilename, 'utf8');
+  const document = new BeautifulDom(contents);
+
+  const tlElements = document.getElementsByTagName("tlLogic");
+
+  for (const tlXml of tlElements) {
+    const name = tlXml.getAttribute("id")!;
+
+    if (name === '278958815') {
+      // This TL refers to the intersection between Camino San Francisco de Paula
+      // and Avenida Astrofísico Francisco Sánchez. Not needed for this.
+      continue;
+    }
+
+    const phases: Phase[] = [];
+
+    for (const phase of tlXml.getElementsByTagName("phase")) {
+      phases.push(new Phase(
+        parseInt(phase.getAttribute("duration")!, 10),
+        phase.getAttribute("state")!
+      ));
+    }
+
+    tlLogicArray.push(new TLLogic(name, phases));
+  }
+
+  return tlLogicArray;
+}
+
+const tl: TLLogic[] = parseTlLogic("assets/anchieta.net.xml");
+
---
+++ src/tl-logic.ts
@@ -0,0 +1,59 @@
+export class Phase {
+  private _duration: number;
+  private _state: string;
+
+  constructor(duration: number, state: string) {
+    this._duration = duration;
+    this._state = state;
+  }
+
+  get duration() {
+    return this._duration;
+  }
+
+  set duration(value: number) {
+    this._duration = value;
+  }
+
+  get state(): string {
+    return this._state;
+  }
+
+  set state(value: string) {
+    this._state = value;
+  }
+}
+
+export class TLLogic {
+  private _name: string;
+  private _phases: Phase[];
+
+  constructor(name: string, phases?: Phase[]) {
+    this._name = name;
+    if (phases) {
+      this._phases = phases;
+    } else {
+      this._phases = [];
+    }
+  }
+
+  pushPhase(phase: Phase) {
+    this.phases.push(phase);
+  }
+
+  get phases(): Phase[] {
+    return this._phases;
+  }
+
+  set phases(value: Phase[]) {
+    this._phases = value;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  set name(value: string) {
+    this._name = value;
+  }
+}
