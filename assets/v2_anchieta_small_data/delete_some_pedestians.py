613a4dade956b08550622d7aa06c6094e6aa3475
Francisco Cruz <cruzelante098@gmail.com>
2020-05-09 21:43:36 +0100

    Fix speed disadjustement and adds instances withoud always green interior lane

---
+++ assets/v2_anchieta_small_data/delete_some_pedestians.py
@@ -0,0 +1,22 @@
+import xml.etree.ElementTree as ET
+from random import random
+
+
+# Open original file
+tree = ET.parse('[OG]peatones_aleatorios.rou.xml')
+
+root = tree.getroot()
+to_delete = []
+
+for i in range(len(root)):
+    if random() > 0.25:
+        to_delete.append(i)
+
+i = 0
+for x in to_delete:
+    del root[x - i]
+    i += 1
+
+print(len(root))
+
+tree.write('peatones_aleatorios.rou.xml')
